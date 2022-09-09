4ffaf3f3aa7c734af77656c687ad892fafa9e77f
Siqian94 <108662763+Siqian94@users.noreply.github.com>
2022-09-09 15:25:22 +0800

    Update enrichr.R

--- enrichr.R
+++ enrichr.R
@@ -3,12 +3,17 @@ library(data.table)
 library("regioneR")
 library(BSgenome.Btaurus.UCSC.bosTau9)
 genomes<-filterChromosomes(getGenome("BSgenome.Btaurus.UCSC.bosTau9"),organism="bosTau")
+## This is an example of enrichment analysis
+## This code calculayed the enrichment of CSE(cattle-specific enhancer) for copy number variable regions.
 
 ####BED file
-Bed<-read.table("CSE.bed.50KB")
+system("slopBed -i CSE.bed -g Bos_taurus.chromosome.length -l 50000 -r 50000 | | sort -k 1,1 -k2,2g -k3,3g>CSE.bed.50KB")
+Bed<-read.table("CSE.bed.50KB") 
 Bed<-toGRanges(Bed, header=FALSE)
 
-Triats <- read.table("ARS_CNV.bed",header=FALSE)
+Triats <- read.table("ARS_CNV.bed",header=FALSE) 
+###ARS_CNV.bed is the file of copy number variable regions(CNVR) which diownloaded from Animal Omics Datbase (AOD)
+
 Triats <-toGRanges(Triats,header=FALSE)
 
 suppressWarnings(pt <-permTest( A=Bed, B=Triats,ntimes=1000, genome=genomes, 
